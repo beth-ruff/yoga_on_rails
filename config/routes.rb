feddac66996e4d888e31f52e14f0fa377aad864b
Beth Ruff <beth.ruff2@gmail.com>
2020-06-16 12:44:49 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {:registrations => 'registrations', :omniauth_callbacks => 'callbacks'}
+
+  root to: 'application#welcome'
+  devise_scope :user do 
+    get 'login', to: 'devise/sessions#new', as: 'login'
+    get 'signup', to: 'devise/registrations#new', as: 'signup'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end 
+
+
+end
