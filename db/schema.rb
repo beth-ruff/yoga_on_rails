1dc77218b78c9a1826df4acbf98c9d775597953b
Beth Ruff <beth.ruff2@gmail.com>
2020-10-21 17:48:33 -0700

    convert database to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_17_214934) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "yoga_class_id"
